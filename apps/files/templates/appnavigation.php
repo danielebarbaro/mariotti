72a3a2e5a8d79ca647ae88c1d180b0240fb0e206
Daniele Barbaro <barbaro.daniele@gmail.com>
2018-09-22 11:37:07 +0200

    Add apps override

---
+++ apps/files/templates/appnavigation.php
@@ -0,0 +1,14 @@
+<div id="app-navigation">
+	<ul class="with-icon">
+		<?php foreach ($_['navigationItems'] as $item) {
+				if ($item['appname'] != 'files_sharing') {
+		?>
+		<li data-id="<?php p($item['id']) ?>" class="nav-<?php p($item['id']) ?>">
+			<a href="<?php p(isset($item['href']) ? $item['href'] : '#') ?>"
+				class="nav-icon-<?php p($item['icon'] !== '' ? $item['icon'] : $item['id']) ?> svg">
+				<?php p($item['name']); ?>
+			</a>
+		</li>
+		<?php } } ?>
+	</ul>
+</div>
